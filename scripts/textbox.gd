93501ce85e64e790300a576ddb39bce969b3c911
Suramya87 <146380955+Suramya87@users.noreply.github.com>
2025-02-27 00:30:07 -0800

    backup save

--- scripts/textbox.gd
+++ scripts/textbox.gd
@@ -3,7 +3,8 @@ extends MarginContainer
 @onready var label = $MarginContainer/Label
 @onready var timer = $LetterDisplayTimer
 
-const MAX_WIDTH = 256
+const MAX_WIDTH = 128
+
 
 var text = ""
 
@@ -28,7 +29,7 @@ func display_text(text_to_display: String):
 		label.autowrap_node = TextServer.AUTOWRAP_WORD
 		await resized
 		await resized
-		custom_minimum_size.y = size.y
+		custom_minimum_size.y = size.y/2
 	
 	global_position.x -= size.x/2
 	global_position.y -= size.y + 12
